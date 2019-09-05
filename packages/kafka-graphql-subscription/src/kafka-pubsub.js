9bb5e89f32a1236b6aa3878a496490c977684c37
Guillaume Bastianelli <guillaume.bastianelli@gmail.com>
2019-09-05 17:29:31 +0200

    new kafka client

--- packages/kafka-graphql-subscription/src/kafka-pubsub.js
+++ packages/kafka-graphql-subscription/src/kafka-pubsub.js
@@ -1,27 +1,32 @@
 // @flow
 import {PubSubEngine} from 'graphql-subscriptions';
-import {ConsumerGroup} from 'kafka-node';
+import {Kafka} from 'kafkajs';
 import uuidv4 from 'uuid/v4';
+import retry from 'async-retry';
 
 import KafkaPubSubIterator from './kafka-pubsub-iterator';
-import buildKafkaConsumer from './kafka/kafka-consumer';
-import KafkaProducer from './kafka/kafka-producer';
+import {logger} from './logger';
+
+const ERROR_TYPES = ['unhandledRejection', 'uncaughtException'];
+const SIGNAL_TRAPS = ['SIGTERM', 'SIGINT', 'SIGUSR2'];
 
 type KafkaPubSubOptionsType = {
 	topic: string,
-	host: string,
+	hosts: string[],
 };
 
 class KafkaPubSub implements PubSubEngine {
 	channelSubscriptions: {[string]: Array<number>}; // {[channel: string]: Array<number>};
 
-	consumer: ConsumerGroup;
+	consumer: Object;
 
 	options: any;
 
-	producer: KafkaProducer;
+	producer: Object;
 
 	subIdCounter: number;
+
+	isReady: boolean = false;
 
 	subscriptionMap: {[number]: [string, Function]};
 
@@ -30,26 +35,100 @@ class KafkaPubSub implements PubSubEngine {
 		this.subscriptionMap = {};
 		this.channelSubscriptions = {};
 		this.subIdCounter = 0;
-		this.consumer = buildKafkaConsumer({
-			host: this.options.host,
-			topicName: this.options.topic,
-			groupName: uuidv4(), // to get message for all node
-			processMessage: message => {
-				const parsedMessage = JSON.parse(message.value.toString());
-				if (parsedMessage.channel) {
-					const {channel, ...payload} = parsedMessage;
-					this.onMessage(parsedMessage.channel, payload);
-					// No channel abstraction, publish over the whole topic
-				} else {
-					this.onMessage(this.options.topic, parsedMessage);
-				}
-			},
-		});
-		this.producer = new KafkaProducer();
+		this._initConsumerAndProducer(this.options.topic, this.options.hosts)
+			.then(() => {
+				logger.info('Connection to Kafka successful.');
+				this.isReady = true;
+			})
+			.catch(e => {
+				logger.error('Error during the connection to Kafka.', e);
+				process.exit(-1);
+			});
 	}
 
+	_initConsumerAndProducer = async (topic: string, hosts: string[]) => {
+		const kafka = new Kafka({
+			clientId: 'kafka-graphql-subscription',
+			brokers: hosts,
+			maxRetryTime: 2,
+		});
+
+		try {
+			const consumer = kafka.consumer({groupId: uuidv4()});
+
+			await consumer.connect();
+			await consumer.subscribe({topic});
+			await consumer.run({
+				eachMessage: async ({topic: topicName, partition, message}) => {
+					logger.debug(
+						'[eachMessage] topic: %s | partition: %s | message: %O',
+						topicName,
+						partition,
+						message,
+					);
+					const parsedMessage = JSON.parse(message.value.toString());
+					if (parsedMessage.channel) {
+						const {channel, ...payload} = parsedMessage;
+						this.onMessage(parsedMessage.channel, payload);
+						// No channel abstraction, publish over the whole topic
+					} else {
+						this.onMessage(this.options.topic, parsedMessage);
+					}
+				},
+			});
+
+			this.consumer = consumer;
+		} catch (e) {
+			logger.error(`Error on creation of the consumer: ${e.message}`);
+			throw e;
+		}
+
+		try {
+			const producer = kafka.producer();
+
+			await producer.connect();
+
+			this.producer = producer;
+		} catch (e) {
+			logger.error(`Error on creation of the consumer: ${e.message}`);
+			throw e;
+		}
+
+		ERROR_TYPES.forEach(type => {
+			process.on(type, async () => {
+				try {
+					logger.error(`process.on ${type}`);
+					await this.consumer.disconnect();
+					await this.producer.disconnect();
+					logger.error(`Consumer and producer disconnected on ${type}.`);
+					process.exit(0);
+				} catch (e) {
+					logger.error('Cannot disconnect producer or consumer: ', e);
+					process.exit(1);
+				}
+			});
+		});
+
+		SIGNAL_TRAPS.forEach(type => {
+			process.once(type, async () => {
+				try {
+					await this.consumer.disconnect();
+					await this.producer.disconnect();
+					logger.error(`Consumer and producer disconnected on ${type}.`);
+				} finally {
+					process.kill(process.pid, type);
+				}
+			});
+		});
+	};
+
 	asyncIterator<T>(triggers: string | Array<string>): AsyncIterator<T> {
-		return new KafkaPubSubIterator<T>(this, triggers);
+		return retry(async () => {
+			if (!this.isReady) {
+				throw new Error('Pubsub is not initialised.');
+			}
+			return new KafkaPubSubIterator<T>(this, triggers);
+		});
 	}
 
 	onMessage(channel: string, message: Object) {
@@ -63,7 +142,15 @@ class KafkaPubSub implements PubSubEngine {
 	}
 
 	async publish(payload: Object) {
-		return this.producer.produce(this.options.topic, payload);
+		return retry(async () => {
+			if (!this.isReady) {
+				throw new Error('Pubsub is not initialised.');
+			}
+			return this.producer.send({
+				topic: this.options.topic,
+				messages: [{value: payload}],
+			});
+		});
 	}
 
 	subscribe(channel: string, onMessage: Function): Promise<number> {
--- packages/kafka-graphql-subscription/src/logger.js
+++ packages/kafka-graphql-subscription/src/logger.js
@@ -1,3 +1,15 @@
-const log4js = require('@log4js-node/log4js-api');
+import log4js from 'log4js';
+
+log4js.configure({
+	appenders: {
+		console: {type: 'console', layout: {type: 'colored'}},
+	},
+	categories: {
+		default: {
+			appenders: ['console'],
+			level: process.env.LOG_LEVEL || 'error',
+		},
+	},
+});
 
 export const logger = log4js.getLogger('kafka-graphql-subscription');
