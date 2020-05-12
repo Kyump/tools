713f32ada5262b432843cc7a5930bac901bef9ef
Guillaume Bastianelli <guillaume.bastianelli@gmail.com>
2020-05-12 16:19:32 +0200

    expose components

--- packages/final-form-generator-mui/src/index.js
+++ packages/final-form-generator-mui/src/index.js
@@ -3,5 +3,15 @@ import MuiForm from './MuiForm';
 
 export {useMuiFormGenerator} from './mui-form-generator';
 export {default as FormComponent} from './FormComponent';
+export {default as FormSelect} from './fields/FormSelect';
+export {default as FormContainer} from './fields/FormContainer';
+export {default as FormFile} from './fields/FormFile';
+export {default as FormRadio} from './fields/FormRadio';
+export {default as FormRadioGroup} from './fields/FormRadioGroup';
+export {default as FormTextField} from './fields/FormTextField';
+export {default as FormCheckbox} from './fields/FormCheckbox';
+export {default as FormCheckboxGroup} from './fields/FormCheckboxGroup';
+export {default as FormCondition} from './fields/FormCondition';
+export {default as FormMultiSelect} from './fields/FormMultiSelect';
 
 export default MuiForm;
